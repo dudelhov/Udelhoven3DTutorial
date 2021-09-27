11d8b64a30169defa4dde4036f9c84a0d235b214
Daniel Udelhoven <dudelhoven@icloud.com>
2021-09-27 13:26:45 -0400

    Revert "Rock Boundary Created"
    This reverts commit e6e00b46c09700c4b7c518240cc3c31102e5f2b4.

--- 3D Tutorial/Assets/Player.cs
+++ 3D Tutorial/Assets/Player.cs
@@ -4,37 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] float _mouseSensitivity = 10f;
-    [SerializeField] float _moveSpeed = 3f;
-    [SerializeField] Camera _cam;
-    float _currentTilt = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Aim();
-        Movement();
-    }
-
-    void Movement(){
-        Vector3 sidewaysMovementVector = transform.right * Input.GetAxis("Horizontal");
-        Vector3 forwardBackwordVector = transform.forward * Input.GetAxis("Vertical");
-        Vector3 movementVector = sidewaysMovementVector + forwardBackwordVector;
         
-        GetComponent<CharacterController>().Move(movementVector * _moveSpeed * Time.deltaTime);
-    }
-
-    void Aim(){
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");
-        transform.Rotate(Vector3.up, mouseX * _mouseSensitivity);
-        _currentTilt -= mouseY * _mouseSensitivity;
-        _currentTilt = Mathf.Clamp(_currentTilt, -90, 90);
-        _cam.transform.localEulerAngles = new Vector3(_currentTilt,0,0);
     }
 }
